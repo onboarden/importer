9fe60ef08d3aa46f1bf1e2f1db013076822e0a78
takasay <takataka4072715@gmail.com>
2022-03-24 15:29:18 +0900

    fix launch url and message source

--- src/importer.ts
+++ src/importer.ts
@@ -1,4 +1,4 @@
-const URL = "https://app.onboarden.io";
+const URL = 'https://app.onboarden.io';
 
 type OnCompletedResult = {
   rows: string[][];
@@ -11,38 +11,38 @@ type LaunchArgs = {
 };
 
 export const launch = ({ templateId, jwt, onCompleted }: LaunchArgs) => {
-  const div = document.createElement("div");
+  const div = document.createElement('div');
   div.style.cssText =
-    "position:fixed;top:0;left:0;width:100%;height:100%;z-index:100;background-color:rgba(100,100,100,0.3);padding: 32px;";
+    'position:fixed;top:0;left:0;width:100%;height:100%;z-index:100;background-color:rgba(100,100,100,0.3);padding: 32px;';
   document.body.appendChild(div);
 
-  const iframe = document.createElement("iframe");
-  iframe.src = encodeURI(`http://localhost:3000/t/${templateId}`);
+  const iframe = document.createElement('iframe');
+  iframe.src = encodeURI(`${URL}/t/${templateId}`);
   iframe.style.cssText =
-    "width:100%;height:100%;background-color:white;margin:auto;border-radius:10px;box-shadow:0,0,15px,rgba(0,0,0,.2)";
+    'width:100%;height:100%;background-color:white;margin:auto;border-radius:10px;box-shadow:0,0,15px,rgba(0,0,0,.2)';
   iframe.onload = () => {
-    iframe.contentWindow?.postMessage({ source: "dazaifu", jwt }, URL);
+    iframe.contentWindow?.postMessage({ source: 'onboarden', jwt }, URL);
   };
   div.appendChild(iframe);
 
-  window.addEventListener("message", (event) => {
+  window.addEventListener('message', (event) => {
     if (event.origin !== URL) return;
 
     switch (event.data.status) {
-      case "succeeded":
+      case 'succeeded':
         onCompleted && onCompleted({ rows: event.data.rows ?? [] });
         document.body.removeChild(div);
         break;
-      case "failed":
+      case 'failed':
         break;
       default:
         break;
     }
   });
 
-  const closeButton = document.createElement("button");
-  closeButton.textContent = "❌";
-  closeButton.style.cssText = "position:fixed;top:12px;right:12px;";
+  const closeButton = document.createElement('button');
+  closeButton.textContent = '❌';
+  closeButton.style.cssText = 'position:fixed;top:12px;right:12px;';
   closeButton.onclick = () => document.body.removeChild(div);
   div.appendChild(closeButton);
 };
